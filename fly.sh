8180f8cb6ffbee00104c679ea6f39f5555121607
Peter Holmberg <peter.holmberg1@saabgroup.com>
2018-05-07 13:54:53 +0200

    Tags

--- fly.sh
+++ fly.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-fly -t oeqcm login -u oeqcm -p oeqcm -c https://concourse.egira.saab.se
-fly set-pipeline --target peter \
+set -e
+fly -t oeqcm login -n oeqcm -p oeqcm -u oeqcm -c https://concourse.egira.saab.se
+fly set-pipeline --target oeqcm \
  --config build-pipeline.yaml --pipeline semver-resource-image --var "private_repo_key=$(cat ~/.ssh/gitlab)"\
  --var "registry=docker.egira.saab.se:5000"  --var "registry-user="  --var "registry-password="\
  --var "git-branch=master" --var "git-repo=ssh://git@git.work:23/oeqcm/semver.git" \
